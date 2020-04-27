418648bc7d3722ddcd280728a477209010dbe6fb
Arkhipenka Andrei <europeoplebelarus@gmail.com>
2020-04-27 22:18:48 +0300

    feat: +

--- src/js/burger-menu.js
+++ src/js/burger-menu.js
@@ -1,62 +1,6 @@
-/*(function () {
-    const
-
-        F2_KEY = 113,
-        ESC_KEY = 27,
-        animationFastDelay = 100,
-        burgerMenu = document.getElementById('btn-burger'),
-        modalMenu = document.getElementById('modal_menu'),
-        btnClose = document.getElementById('close');
-
-    let
-        storage = window.localStorage,
-
-        isOpened = function isOpened() {
-            return "1" == storage.getItem('mainMenuOpened');
-        },
-
-        openMenu = function openMenu() {
-            storage.setItem('mainMenuOpened', 1);
-
-            modalMenu.style.display = 'block';
-            modalMenu.style.width = '0';
-
-            setTimeout(function () {
-                modalMenu.style.display = null;
-                modalMenu.style.width = null;
-                modalMenu.classList.add('active');
-            }, animationFastDelay);
-        },
-
-        closeMenu = function closeMenu() {
-            storage.setItem('mainMenuOpened', 0);
-
-            modalMenu.style.display = 'block';
-            modalMenu.style.width = '0';
-
-            setTimeout(function () {
-                modalMenu.style.display = null;
-                modalMenu.style.width = null;
-                modalMenu.classList.remove('active');
-            }, animationFastDelay);
-        };
-
-    burgerMenu.addEventListener('click', (event) => {
-        event.stopPropagation();
-        openMenu();
-    });
-
-    btnClose.addEventListener('click', (event) => {
-        event.stopPropagation();
-        closeMenu();
-    });
-
-    document.onkeydown = (event) => {
-        if (isOpened() && ESC_KEY === event.keyCode) {
-            closeMenu();
-        } else if (!isOpened() && F2_KEY === event.keyCode) {
-            openMenu();
-        }
-    };
-})();
-*/
+const burger_menu = document.getElementById("btn-burger");
+const modal_menu = document.getElementById("modal-menu");
+burger_menu.addEventListener("click", e => {
+    event.target.classList.toggle("active");
+    modal_menu.classList.toggle("active");
+});
--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,22 @@
-console.log('main.js');
+const checbox = document.getElementById("switch-checkbox");
+const categories = document.getElementsByClassName("categories");
+const btn_start = document.getElementById("btn-start");
+checbox.addEventListener("change", OnChange);
+function OnChange(e) {
+    if (e.currentTarget.checked) {
+        localStorage.setItem("trainMode", "true");
+        btn_start.classList.add("btn-active");
+    } else {
+        localStorage.setItem("trainMode", "false");
+        btn_start.classList.remove("btn-active");
+    }
+
+    console.log(e.currentTarget.checked);
+    console.log(localStorage.getItem("trainMode"));
+}
+
+/*categories.addEventListener("click", (event) => {
+    
+})*/
+
+console.log("main.js");
